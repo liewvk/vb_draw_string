6ba360137ab1a4340a7a6ba33a76d0c1f7078f4a
Dr. Liew vk <liewvk@yahoo.com>
2026-08-25 19:06:55 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        picBox = New PictureBox()
+        btnDrawText = New Button()
+        btnClear = New Button()
+        CType(picBox, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' picBox
+        ' 
+        picBox.Location = New Point(91, 48)
+        picBox.Name = "picBox"
+        picBox.Size = New Size(597, 337)
+        picBox.TabIndex = 0
+        picBox.TabStop = False
+        ' 
+        ' btnDrawText
+        ' 
+        btnDrawText.Location = New Point(177, 402)
+        btnDrawText.Name = "btnDrawText"
+        btnDrawText.Size = New Size(157, 36)
+        btnDrawText.TabIndex = 1
+        btnDrawText.Text = "Draw Text"
+        btnDrawText.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(405, 402)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(157, 36)
+        btnClear.TabIndex = 1
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnClear)
+        Controls.Add(btnDrawText)
+        Controls.Add(picBox)
+        Name = "Form1"
+        Text = "Form1"
+        CType(picBox, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents picBox As PictureBox
+    Friend WithEvents btnDrawText As Button
+    Friend WithEvents btnClear As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,237 @@
+﻿Public Class Form1
+
+    Private showTextExample As Boolean = False
+
+
+    Private Sub btnDrawText_Click(
+        sender As Object,
+        e As EventArgs) Handles btnDrawText.Click
+
+        showTextExample = True
+
+        picBox.Invalidate()
+
+    End Sub
+
+
+    Private Sub btnClear_Click(
+        sender As Object,
+        e As EventArgs) Handles btnClear.Click
+
+        showTextExample = False
+
+        picBox.Invalidate()
+
+    End Sub
+
+
+    Private Sub picBox_Paint(
+        sender As Object,
+        e As PaintEventArgs) Handles picBox.Paint
+
+        If Not showTextExample Then
+            Return
+        End If
+
+        Dim g As Graphics = e.Graphics
+
+        g.Clear(Color.White)
+
+        DrawBasicText(g)
+
+        DrawStyledText(g)
+
+        DrawCentredText(g)
+
+        DrawTextInRectangle(g)
+
+    End Sub
+
+
+    ' ------------------------------------------------------------
+    ' Basic text
+    ' ------------------------------------------------------------
+
+    Private Sub DrawBasicText(g As Graphics)
+
+        Using font As New Font(
+            "Segoe UI",
+            14,
+            FontStyle.Regular)
+
+            Using brush As New SolidBrush(Color.Black)
+
+                g.DrawString(
+                    "Hello, VB 2026!",
+                    font,
+                    brush,
+                    20,
+                    20)
+
+            End Using
+
+        End Using
+
+    End Sub
+
+
+    ' ------------------------------------------------------------
+    ' Bold, italic, and underlined text
+    ' ------------------------------------------------------------
+
+    Private Sub DrawStyledText(g As Graphics)
+
+        Using boldFont As New Font(
+            "Arial",
+            12,
+            FontStyle.Bold)
+
+            Using italicFont As New Font(
+                "Arial",
+                12,
+                FontStyle.Italic)
+
+                Using underlineFont As New Font(
+                    "Arial",
+                    12,
+                    FontStyle.Underline)
+
+                    Using brush As New SolidBrush(
+                        Color.DarkBlue)
+
+                        g.DrawString(
+                            "Bold",
+                            boldFont,
+                            brush,
+                            20,
+                            60)
+
+                        g.DrawString(
+                            "Italic",
+                            italicFont,
+                            brush,
+                            20,
+                            90)
+
+                        g.DrawString(
+                            "Underline",
+                            underlineFont,
+                            brush,
+                            20,
+                            120)
+
+                    End Using
+
+                End Using
+
+            End Using
+
+        End Using
+
+    End Sub
+
+
+    ' ------------------------------------------------------------
+    ' Centre text using MeasureString
+    ' ------------------------------------------------------------
+
+    Private Sub DrawCentredText(g As Graphics)
+
+        Dim text As String =
+            "Centred Title"
+
+
+        Using font As New Font(
+            "Segoe UI",
+            16,
+            FontStyle.Bold)
+
+            Using brush As New SolidBrush(
+                Color.Navy)
+
+                Dim textSize As SizeF =
+                    g.MeasureString(
+                        text,
+                        font)
+
+
+                Dim x As Single =
+                    (picBox.ClientSize.Width -
+                     textSize.Width) / 2
+
+
+                Dim y As Single = 170
+
+
+                g.DrawString(
+                    text,
+                    font,
+                    brush,
+                    x,
+                    y)
+
+            End Using
+
+        End Using
+
+    End Sub
+
+
+    ' ------------------------------------------------------------
+    ' Centre text inside a rectangle
+    ' ------------------------------------------------------------
+
+    Private Sub DrawTextInRectangle(
+        g As Graphics)
+
+        Dim rect As New Rectangle(
+            50,
+            230,
+            350,
+            90)
+
+
+        Using backgroundBrush As New SolidBrush(
+            Color.SteelBlue)
+
+            g.FillRectangle(
+                backgroundBrush,
+                rect)
+
+        End Using
+
+
+        Using font As New Font(
+            "Segoe UI",
+            12,
+            FontStyle.Bold)
+
+            Using textBrush As New SolidBrush(
+                Color.White)
+
+                Using format As New StringFormat()
+
+                    format.Alignment =
+                        StringAlignment.Center
+
+                    format.LineAlignment =
+                        StringAlignment.Center
+
+
+                    g.DrawString(
+                        "Horizontally and Vertically Centred",
+                        font,
+                        textBrush,
+                        rect,
+                        format)
+
+                End Using
+
+            End Using
+
+        End Using
+
+    End Sub
+
+End Class
+
